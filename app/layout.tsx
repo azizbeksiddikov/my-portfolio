a3dac707d33a47f24d7d65d69a6ebe3893955c9d
azizbeksiddikov <azbeksid@gmail.com>
2025-01-01 02:06:53 +0900

    feat: add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { AppSidebar } from "@/components/AppSidebar";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export const metadata = {
@@ -28,6 +29,7 @@ export default function RootLayout({
           <main className="flex-1 min-h-screen">{children}</main>
         </ThemeProvider>
         {/* Analytics */}
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
